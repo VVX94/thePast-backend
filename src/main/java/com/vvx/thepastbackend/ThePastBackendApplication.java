d54a7a84d42e5f6e71a4b9d578de1ae850ded43d
Wang <1535412670@qq.com>
2025-04-02 21:06:23 +0800

    Initial commit

---
+++ src/main/java/com/vvx/thepastbackend/ThePastBackendApplication.java
@@ -0,0 +1,13 @@
+package com.vvx.thepastbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePastBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThePastBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vvx/thepastbackend/ThePastBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vvx.thepastbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThePastBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
